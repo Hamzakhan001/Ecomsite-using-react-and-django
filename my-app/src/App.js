52c196ca6e5312cba856b33a3f4bbbd42724b984
Hamzakhan001 <58425510+Hamzakhan001@users.noreply.github.com>
2021-12-18 01:05:12 +0500

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,27 @@
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import HomeScreen  from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
     <div >
+      <Router>
+
+      
       <Header/>
+      <main className="py-3">
+        <Container>
+        <Route path='/' component={HomeScreen} exact/>
+        <Route path='/product/:id' component={ProductScreen} />
+        </Container>
         
-      <Footer />
+      </main>
+      <Footer/>
+      </Router>
+
     </div>
   );
 }
